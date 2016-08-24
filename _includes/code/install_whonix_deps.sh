c0a7c1714158be8413e6188251823881b96719c7
turquoise <treefrog@riseup.net>
2016-08-24 09:25:06 +0000

    add user to both libvrt and kvm groups

--- _includes/code/install_whonix_deps.sh
+++ _includes/code/install_whonix_deps.sh
@@ -3,11 +3,11 @@
 ################################################################################
 # install dependencies for whonix and kvm.
 ################################################################################
-sudo pacman -S libvirt qemu virt-manager dnsmasq ebtables
+
+sudo pacman -S libvirt qemu virt-manager dnsmasq ebtables bridge-utils
 
 sudo systemctl enable libvirtd.service
-sudo usermod -aG kvm $(whoami)
+sudo usermod -aG kvm,libvirt $(whoami)
 
 # reboot here.
 echo "reboot your computer now."
-

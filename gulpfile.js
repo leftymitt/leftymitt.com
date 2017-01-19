440ad71465f6e1c8f8ee294031cc589c42358dbc
turquoise <treefrog@riseup.net>
2017-01-19 13:39:12 +0000

    set gulpfile to watch markdown files and rebuild-on-change

--- gulpfile.js
+++ gulpfile.js
@@ -370,10 +370,10 @@ gulp.task('watch', function() {
 
   watch(
     [
-      '_posts/**', '*.html', '_layouts/*', '_config.yml', '_includes/**',
-      'resources/*', '_resources/**', '_drafts/**', 'media/*', '_media/*',
-      'blog/*', 'projects/*', '_projects/**', '_data/**', 'feed.xml',
-      '404.html'
+      '_posts/**', '*.md', '*.html', '_layouts/*', '_config.yml',
+      '_includes/**', 'resources/*', '_resources/**', '_drafts/**', 'media/*',
+      '_media/*', 'blog/*', 'projects/*', '_projects/**', '_data/**',
+      'feed.xml', '404.html'
     ], 
     function() {
       run_sequence('build', 'reload'); 
